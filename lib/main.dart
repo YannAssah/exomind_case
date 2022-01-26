51f9c2bb357d3e643bf4378c5f8f017de9e8339a
Yann Assah <assah.yann@gmail.com>
2022-01-26 01:29:30 +0100

    First page added, UI elements construction

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:exomind_case/views/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,36 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Test"),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
-        ),
-      ),
     );
   }
 }
---
+++ lib/views/homepage.dart
@@ -0,0 +1,53 @@
+import 'package:exomind_case/views/forecast.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String homeLogo = 'assets/images/sunny.gif';
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var sWidth = screenSize.width;
+    var sHeight = screenSize.height;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FadeInImage(
+              placeholder: AssetImage(homeLogo),
+              image: AssetImage(homeLogo),
+              width: 400,
+              height: 400,
+            ),
+            //Image.asset(homeLogo),
+            Container(
+              child: ElevatedButton(
+                child: Text('Aller à l\'écran suivant'),
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Forecast(),
+                    ),
+                  )
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
