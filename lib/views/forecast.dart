baa58428da808e9942700f42f66e28c83d6879d5
Yann Assah <assah.yann@gmail.com>
2022-01-26 01:27:51 +0100

    First page added, ui elements construction

---
+++ lib/views/forecast.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Forecast extends StatefulWidget {
+  Forecast({Key? key}) : super(key: key);
+
+  @override
+  State<Forecast> createState() => _ForecastState();
+}
+
+class _ForecastState extends State<Forecast> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
